339ecfb53259826e06d8b36cb1fc5125f578a372
Greg Morgan <gregm9999@gmail.com>
2018-10-26 21:12:33 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,6 +11,10 @@ def turn(board)
   puts "Please enter 1-9"
 end
 
+def input_to_index(input)
+  index=input.to_i-1
+end
+
 def valid_move?(board, index)
   if index <8 && index>=0
     return true
