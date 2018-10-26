6e4675a4b7c720e0f7cbab22a7bd18fcb225b953
Greg Morgan <gregm9999@gmail.com>
2018-10-26 20:58:39 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,3 +2,13 @@
 def display_board(board)
   puts board
 end
+
+def valid_move?(board, index)
+  if index <8 && index>=0
+    return true
+  else 
+    return false
+elsif board[index] !="X" || board[index]!="0"
+  return true
+  
+end
